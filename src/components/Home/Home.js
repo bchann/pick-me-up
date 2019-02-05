afd2a29685ef2f7fa0588ad8c3b62429a8194965
Brian Chan <alto1020@gmail.com>
2019-02-05 01:38:58 -0800

    Home screen skeleton

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div className='Home'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/components/Home/Home.js</code> and save to reload.
-          </p>
-          <a className='btn btn-primary' href='/test' role='button'>
-            Route Test
-          </a>
-        </header>
+      <div className='home'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <div className='col'>
+              <h1>Pick Me Up</h1>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-10 col-md-8'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text'>
+                    <i className='material-icons'>search</i>
+                  </span>
+                </div>
+                <input type='text' className='form-control' placeholder='Destination' aria-label='Destination' />
+                <div className='input-group-append'>
+                  <a className='btn btn-outline-secondary' role='button' href='/test'>
+                    Go
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col'>
+              <h4>Recent Searches</h4>
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-8 col-md-6'>
+              <ul className='list-group'>
+                <a href='#' class='list-group-item list-group-item-action'>
+                  Geisel
+                </a>
+                <a href='#' class='list-group-item list-group-item-action'>
+                  Costco
+                </a>
+                <a href='#' class='list-group-item list-group-item-action'>
+                  McDonald's
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
